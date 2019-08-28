c7212618798984793388fb293c77917e19a8fa81
Petter Häggholm <petter@petterhaggholm.net>
2019-08-28 14:16:16 -0700

    fix: excessive stack depth [https://github.com/typeorm/typeorm/issues/4427]

--- src/find-options/FindConditions.ts
+++ src/find-options/FindConditions.ts
@@ -1,8 +1,11 @@
-import {FindOperator} from "./FindOperator";
+import { FindOperator } from "./FindOperator";
 
 /**
  * Used for find operations.
  */
 export type FindConditions<T> = {
-    [P in keyof T]?: FindConditions<T[P]>|FindOperator<FindConditions<T[P]>>;
+    // @petter: https://github.com/typeorm/typeorm/issues/4427
+    [P in keyof T]?: T[P] extends never
+        ? FindConditions<T[P]> | FindOperator<FindConditions<T[P]>>
+        : FindConditions<T[P]> | FindOperator<FindConditions<T[P]>>;
 };
